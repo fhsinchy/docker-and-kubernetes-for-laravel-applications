44a20172ecaf5a8c3a5f263ec0be9d58899143d5
Farhan Hasin Chowdhury <shovik.is.here@gmail.com>
2022-02-21 00:08:44 +0600

    Fixed all remaining issues

--- database/docker-entrypoint-initdb.d/dump-question_board-202202202300.sql
+++ database/docker-entrypoint-initdb.d/dump-question_board-202202202300.sql
@@ -19,6 +19,8 @@
 -- Table structure for table `answers`
 --
 
+USE question_board;
+
 DROP TABLE IF EXISTS `answers`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
